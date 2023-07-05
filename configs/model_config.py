568e8499618ba7344706073a330d56f8a514cd29
stc2001 <46279716+stc2001@users.noreply.github.com>
2023-07-05 18:24:59 +0800

    Update model_config.py

--- configs/model_config.py
+++ configs/model_config.py
@@ -16,7 +16,7 @@ embedding_model_dict = {
     "ernie-tiny": "nghuyong/ernie-3.0-nano-zh",
     "ernie-base": "nghuyong/ernie-3.0-base-zh",
     "text2vec-base": "shibing624/text2vec-base-chinese",
-    "text2vec": "GanymedeNil/text2vec-large-chinese",
+    "text2vec": "kaggle/working/text2vec-large-chinese",
     "m3e-small": "moka-ai/m3e-small",
     "m3e-base": "moka-ai/m3e-base",
 }
@@ -66,7 +66,7 @@ llm_model_dict = {
     },
     "chatglm2-6b-int4": {
         "name": "chatglm2-6b-int4",
-        "pretrained_model_name": "THUDM/chatglm2-6b-int4",
+        "pretrained_model_name": "kaggle/working/chatglm2-6b-int4",
         "local_model_path": None,
         "provides": "ChatGLM"
     },
@@ -122,7 +122,7 @@ llm_model_dict = {
 }
 
 # LLM 名称
-LLM_MODEL = "chatglm-6b"
+LLM_MODEL = "chatglm2-6b-int4"
 # 量化加载8bit 模型
 LOAD_IN_8BIT = False
 # Load the model with bfloat16 precision. Requires NVIDIA Ampere GPU.
